25a01a932205cff89f3e1e4bdddfc9219913c771
yijordan <yij@uchicago.edu>
2025-03-07 02:36:49 +0000

    create

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -2,4 +2,11 @@ class FormController < ApplicationController
   def add_item
     render({ :template => "form_templates/submission"})
   end
+  
+  def create
+    link_url = params.fetch("link")
+    link_description = params.fetch("description")
+    thumbnail_url = params.fetch("image")
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "items", :action => "index" })
 
   get("/backdoor", { :controller => "form", :action => "add_item"})
+
+  get("/create_item", { :controller => "form", :action => "create"})
 end
