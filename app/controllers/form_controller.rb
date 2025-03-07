e5cb679224dcf5b7153c06a01778d7f0b99e5ef4
yijordan <yij@uchicago.edu>
2025-03-07 02:46:46 +0000

    it works!

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -7,6 +7,13 @@ class FormController < ApplicationController
     link_url = params.fetch("link")
     link_description = params.fetch("description")
     thumbnail_url = params.fetch("image")
+
+    tired = Item.new
+    tired.link_url = link_url
+    tired.link_description = link_description
+    tired.thumbnail_url = thumbnail_url
+    tired.save
+
     redirect_to("/")
   end
 end
